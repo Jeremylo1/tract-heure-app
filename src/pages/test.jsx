024fb15864503418115db9b65fb5f9a17ca34cad
Jeremylo1 <Jeremylo1@users.noreply.github.com>
2023-07-25 12:37:19 -0400

    Page de test

---
+++ src/pages/test.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import 'bulma/css/bulma.min.css'
+
+function Test() {
+  return (
+    <div>
+      <h1> Page de test </h1>
+    </div>
+  )
+}
+
+export default Test
