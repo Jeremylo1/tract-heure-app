a0034eec67d25f0b36e1e7439840bd32c99b02dd
Elliissa01 <isa.herode@gmail.com>
2023-07-25 12:03:17 -0400

    Librairies et routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,23 @@
+/*Librairies*/
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
-import App from './components/App'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+/*Pages*/
+import Home from './pages/home'
+import Inventory from './pages/inventory'
+import Calendar from './pages/calendar'
+/*Components*/
+import Header from './components/header'
+import Footer from './components/footer'
+import Error from './components/error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
