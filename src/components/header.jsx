3b1b903ae42f9c2c5b240124f26972cfcb6fac88
Elliissa01 <isa.herode@gmail.com>
2023-07-25 13:06:48 -0400

    CSS

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+/*CSS*/
+import styled from 'styled-components'
+import 'bulma/css/bulma.min.css'
+
+/*function Header() {
+    return (
+        <div>
+            <Link to="/">
+
+            </Link>
+        </div>
+}*/
