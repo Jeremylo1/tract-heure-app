db49eb2705cd1d0429890de8e319fb6bf752e61a
Elliissa01 <isa.herode@gmail.com>
2023-07-25 13:36:44 -0400

    Logo et header + Placeholder pour les pages

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,30 @@
-/*CSS*/
+import { Link } from 'react-router-dom'
+import Logo from '../assets/logo-tract-heure.svg'
 import styled from 'styled-components'
 import 'bulma/css/bulma.min.css'
 
-/*function Header() {
-    return (
-        <div>
-            <Link to="/">
+//Style du logo.
+const StyledLogo = styled.img`
+  width: 110px;
+`
 
-            </Link>
-        </div>
-}*/
+//Style de l'entête.
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+//Entête pour naviguer entre les pages.
+function Header() {
+  return (
+    <NavContainer>
+      <Link to="/">
+        <StyledLogo src={Logo} alt="Logo" />
+      </Link>
+    </NavContainer>
+  )
+}
+
+export default Header
