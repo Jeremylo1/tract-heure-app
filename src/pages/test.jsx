f424161c684cee0884d11ea0363f12a2a7c9f9b3
Jeremylo1 <Jeremylo1@users.noreply.github.com>
2023-08-02 12:16:47 -0400

    restructuration uilisation API Hasura

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -3,12 +3,33 @@ import { useState, useEffect } from 'react'
 
 function Test() {
   const [title, setTitle] = useState('') // Le titre du todo à ajouter
+  const [selectedUserId, setSelectedUserId] = useState('') // Pour stocker l'ID de l'utilisateur sélectionné
 
-  // Permet de récupérer les données depuis Hasura.
-  const { data, isLoading, error } = useFetchHasura(
+  // État pour suivre si les données doivent être affichées en format JSON ou non
+  const [showJSON, setShowJSON] = useState(false)
+
+  // Permet de récupérer les données todos depuis Hasura.
+  const { data, isLoading, error, reload } = useFetchHasura(
     'https://champion-tiger-15.hasura.app/v1/graphql',
-    '{todos{id title is_public is_completed user_id}}',
+    `{todos{id title is_public is_completed user_id user { name }}}`,
   )
+
+  // Permet de récupérer les données utilisateurs depuis Hasura.
+  const {
+    data: usersData,
+    isLoading: usersLoading,
+    error: usersError,
+  } = useFetchHasura(
+    'https://champion-tiger-15.hasura.app/v1/graphql',
+    `{users{id name}}`,
+  )
+
+  // Permet de sélectionner le premier utilisateur par défaut et de réinitialiser l'ID sélectionné si la liste des utilisateurs change.
+  useEffect(() => {
+    if (usersData && usersData.users.length > 0) {
+      setSelectedUserId(usersData.users[0].id)
+    }
+  }, [usersData])
 
   // Permet d'envoyer une requête à Hasura.
   const { doMutation } = useMutationHasura(
@@ -25,39 +46,70 @@ function Test() {
       }
     `
 
-    await doMutation(mutation, { title, userId: '1' })
+    await doMutation(mutation, { title, userId: selectedUserId })
     setTitle('')
+    setSelectedUserId('') // Réinitialiser l'ID sélectionné
+    reload() // Appeler reload pour déclencher un rechargement des todos
   }
 
   // Affichage de la page de test avec le formulaire et les données récupérées depuis Hasura.
   return (
     <div>
-      <h1> Page de test </h1>
-      {/* Formulaire pour l'ajout de todo */}
+      <h1>Page de test</h1>
+      <button onClick={() => setShowJSON(!showJSON)}>
+        {showJSON ? 'Afficher au format normal' : 'Afficher au format JSON'}
+      </button>
       <div>
         <input
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder="Ajouter un todo"
         />
+        {usersLoading ? (
+          <div>Chargement des utilisateurs...</div>
+        ) : usersError ? (
+          <div>Erreur lors du chargement des utilisateurs</div>
+        ) : (
+          <select
+            value={selectedUserId}
+            onChange={(e) => setSelectedUserId(e.target.value)}
+          >
+            {usersData.users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        )}
         <button onClick={handleAddTodo}>Ajouter</button>
-      </div>
-      {error && <div>Erreur lors du chargement des données</div>}
-      {isLoading ? (
-        <div>Chargement...</div>
-      ) : (
-        data && (
+        {error ? (
+          <div>Erreur lors du chargement des données</div>
+        ) : isLoading ? (
+          <div>Chargement...</div>
+        ) : showJSON ? (
+          // Si showJSON est vrai, affiche les données au format JSON
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        ) : (
+          // Si showJSON est faux, affiche les données formatées
           <div>
-            <h2>Data:</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            <h2>Todos:</h2>
+            <ul>
+              {data.todos.map((todo) => (
+                <li key={todo.id}>
+                  {todo.title} (Par : {todo.user.name}) -{' '}
+                  {todo.is_completed ? 'Complété' : 'Non Complété'}
+                </li>
+              ))}
+            </ul>
           </div>
-        )
-      )}
+        )}
+      </div>
+      {/* Le reste du code d'affichage reste le même */}
     </div>
   )
 }
 
-// Hook permettant d'envoyer une requête de mutation à Hasura.
+// Hook pour envoyer une requête de mutation à Hasura
 function useMutationHasura(url) {
   const doMutation = async (mutation, variables) => {
     try {
@@ -86,14 +138,16 @@ function useMutationHasura(url) {
   return { doMutation }
 }
 
-// Hook permettant de récupérer les données depuis Hasura.
+// Hook pour récupérer des données depuis Hasura
 function useFetchHasura(url, query) {
   const [data, setData] = useState(null)
   const [isLoading, setLoading] = useState(true)
   const [error, setError] = useState(false)
+  const [reloadTimestamp, setReloadTimestamp] = useState(0) // Nouvel état pour le timestamp de rechargement
+  const reload = () => setReloadTimestamp(Date.now()) // Fonction pour déclencher un rechargement
 
   useEffect(() => {
-    if (!url || !query) return
+    if (!url || !query) return // Ne fait rien si l'url ou la requête sont indéfinis
     setLoading(true)
 
     async function fetchData() {
@@ -123,9 +177,9 @@ function useFetchHasura(url, query) {
     }
 
     fetchData()
-  }, [url, query])
+  }, [url, query, reloadTimestamp]) // Ajoutez reloadTodos ici
 
-  return { isLoading, data, error }
+  return { isLoading, data, error, reload }
 }
 
 export default Test
