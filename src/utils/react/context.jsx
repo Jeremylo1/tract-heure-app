ffb4e6aac3ceb5303f984ad74caad94c0f6aa206
Elliissa01 <isa.herode@gmail.com>
2023-08-04 13:19:15 -0400

    Commentaire context.jsx

--- src/utils/react/context.jsx
+++ src/utils/react/context.jsx
@@ -1,21 +1,19 @@
 import { createContext, useState, useEffect } from 'react'
 
+//Contexte pour gérer la connexion.
 export const AuthContext = createContext()
 
+//Fonction pour gérer la connexion.
 export function AuthProvider({ children }) {
   const [isConnected, setIsConnected] = useState(false)
 
+  //Vérifie si l'utilisateur est connecté.
   useEffect(() => {
     const connected = localStorage.getItem('isConnected') === 'true'
     setIsConnected(connected)
   }, [])
 
-  // const toggleLog = () => {
-  //   setIsConnected(!isConnected)
-  // }
-
   //Fonction pour se connecter.
-
   const setLogin = (userType) => {
     setIsConnected(true)
     localStorage.setItem('userType', userType)
