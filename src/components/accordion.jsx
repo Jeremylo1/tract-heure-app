cd8636f731fdcaa5b2180e6d6b873999f61a4b75
Elliissa01 <isa.herode@gmail.com>
2023-08-04 23:04:58 -0400

    Accordéon dans accordion.jsx et burger.jsx

--- src/components/accordion.jsx
+++ src/components/accordion.jsx
@@ -1,7 +1,9 @@
+import React from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 
-//Style de l'accordéon.
-const StyledAccordion = styled.div`
+//Style du bouton de l'accordéon.
+const StyledButton = styled.button`
   background-color: #eee;
   color: #444;
   cursor: pointer;
@@ -12,50 +14,50 @@ const StyledAccordion = styled.div`
   outline: none;
   font-size: 15px;
   transition: 0.4s;
+
   &:hover {
     background-color: #ccc;
   }
-  &:after {
-    content: '\002B';
-  color: #777;
-  font-weight: bold;
-  float: right;
-  margin-left: 5px;
+`
+
+//Style de l'icône du bouton de l'accordéon.
+const StyledIcon = styled.span`
+  &::before {
+    content: ${(props) => (props.active ? '"\\2212"' : '"\\002B"')};
+    color: #777;
+    font-weight: bold;
+    float: right;
+    margin-left: 5px;
   }
 `
 
-function Accordion() {
+//Style du panneau.
+const StyledPanel = styled.div`
+  padding: 0 18px;
+  background-color: white;
+  max-height: ${(props) => (props.open ? '1000px' : '0')};
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+`
+
+//Composant de l'accordéon.
+function Accordion({ title, content }) {
+  //Pour savoir si le panneau est ouvert ou non.
+  const [isOpen, setIsOpen] = useState(false)
+  //Pour ouvrir ou fermer le panneau.
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div>
-      <button className="accordion">Fleur</button>
-      <div className="panel">
-        <p>
-          En biologie, chez toutes sortes de « plantes à fleur », la fleur
-          constitue l'organe de la reproduction sexuée et l'ensemble des «
-          enveloppes » qui l'entourent. Après la pollinisation, la fleur est
-          fécondée et se transforme en fruit contenant les graines.
-        </p>
-      </div>
-
-      <button className="accordion">Graine</button>
-      <div className="panel">
-        <p>
-          Dans le cycle de vie des « plantes à graines », appelées
-          spermatophytes, la graine est la structure qui contient et protège
-          l'embryon végétal. Elle est souvent contenue dans un fruit qui permet
-          sa dissémination.
-        </p>
-      </div>
-
-      <button className="accordion">Fruit</button>
-      <div className="panel">
-        <p>
-          Le fruit, en botanique, est l'organe végétal contenant une ou
-          plusieurs graines. Caractéristique des Angiospermes, il succède à la
-          fleur par transformation du pistil. La paroi de l'ovaire forme le
-          péricarpe du fruit et l'ovule donne la graine.
-        </p>
-      </div>
+      <StyledButton onClick={toggleAccordion}>
+        {title}
+        <StyledIcon active={isOpen} />
+      </StyledButton>
+      <StyledPanel open={isOpen}>
+        <p>{content}</p>
+      </StyledPanel>
     </div>
   )
 }
