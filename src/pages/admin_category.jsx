bd11b5c5f13786056f8b6c724f4af245c26e7d98
Elliissa01 <isa.herode@gmail.com>
2023-08-14 19:32:41 -0400

    Affichage des catégories dans un tableau (admin)

--- src/pages/admin_category.jsx
+++ src/pages/admin_category.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, useRowSelect } from 'react-table'
 import { ScreenContext } from '../utils/react/context'
 import { Link } from 'react-router-dom'
 import { useFetchHasura } from '../utils/react/hooks'
@@ -11,7 +11,7 @@ import {
   COLUMN_NAME,
 } from '../utils/database/query'
 
-//Page d'accueil pour l'administrateur.
+//Page de gestion des catégories.
 function AdminCategory() {
   //Pour savoir si c'est la première fois qu'on charge les données.
   const [firstLoading, setFirstLoading] = useState(true)
@@ -34,6 +34,66 @@ function AdminCategory() {
   useEffect(() => {
     setFirstLoading(false)
   }, [])
+
+  //Permet de créer le tableau des catégories.
+  function tableCategory() {
+    return (
+      <div>
+        {category_loading ? (
+          <div>Chargement des catégories ...</div>
+        ) : category_error ? (
+          <div>Erreur lors du chargement des catégories !</div>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Catégorie</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {category_data[TABLE_CATEGORY].map((category) => (
+                <tr key={`${category[COLUMN_NAME]}-${category[COLUMN_ID]}`}>
+                  <td>{category[COLUMN_NAME]}</td>
+                  <td>
+                    <button>S</button>
+                    <button>M</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    )
+  }
+
+  //Affichage selon le type d'appareil.
+  return (
+    <div>
+      {isMobile ? (
+        /* DESIGN POUR MOBILE */
+        <div>
+          <div className="columns-mobile">
+            <div className="columns-mobile-size">
+              <h1>Gestion des catégories</h1>
+              <div>{tableCategory()}</div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        /* DESIGN POUR TABLETTE ET ORDINATEUR */
+        <div>
+          <div className="columns-tablet-desktop">
+            <div className="columns-tablet-desktop-size">
+              <h1>Gestion des catégories</h1>
+              <div>{tableCategory()}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default AdminCategory
