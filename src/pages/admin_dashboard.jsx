82fbe21069393cfc041f23a1cf6ea2e715727e1e
Jeremylo1 <Jeremylo1@users.noreply.github.com>
2023-08-22 14:49:12 -0400

    ajout marging right (mr-2) boutons admin_dashboard

--- src/pages/admin_dashboard.jsx
+++ src/pages/admin_dashboard.jsx
@@ -25,13 +25,16 @@ function Dashboard() {
         >
           <h1>Tableau de bord</h1>
           <div className="box-container">
-            <Link to="/admin/category" className="box square-box category-box">
+            <Link
+              to="/admin/category"
+              className="box square-box category-box mr-2" //mr-2 pour margin-right: 2rem;
+            >
               <div className="box-content">Gestion des catégories</div>
             </Link>
 
             <Link
               to="/admin/machinery"
-              className="box square-box machinery-box"
+              className="box square-box machinery-box mr-2" //mr-2 pour margin-right: 2rem;
             >
               <div className="box-content">Gestion de la machinerie</div>
             </Link>
