7543a2d50e378c045374baf4cf40e518865b4117
Elliissa01 <isa.herode@gmail.com>
2023-08-23 13:39:41 -0400

    form_addreservation.jsx

--- src/forms/form_addreservation.jsx
+++ src/forms/form_addreservation.jsx
@@ -1,1 +1,14 @@
 import React, { useState } from 'react'
+
+//Formulaire de réservation d'une machine.
+function FormAddReservation() {
+  return (
+    <div>
+      <p>
+        Insérer formulaire de réservation et toutes fonctions correspondantes
+      </p>
+    </div>
+  )
+}
+
+export default FormAddReservation
